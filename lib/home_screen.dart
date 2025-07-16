b6bdd8eedfd3f76add9b8677c2b58f8d8f9f79ae
Tushar27908 <tushargupta@gmail.com>
2025-07-16 19:59:54 +0530

    Assigment 2

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -20,117 +20,124 @@ class HomeScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.green.shade900,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          height: 250,
-          width: double.infinity,
+      body: Container(
+        padding: EdgeInsets.all(5),
+        margin: EdgeInsets.all(5),
+        decoration: BoxDecoration(
           color: Colors.black,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Icon(Icons.phone, color: Colors.grey),
-                    Text("86196****", style: TextStyle(color: Colors.grey)),
-                  ],
-                ),
-              ),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.orange.withValues(alpha: 0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        height: 250,
+        width: double.infinity,
 
-              Row(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
+                  Icon(Icons.phone, color: Colors.grey),
+                  Text("86196****", style: TextStyle(color: Colors.grey)),
+                ],
+              ),
+            ),
+
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    radius: 53,
+                    backgroundColor: Colors.orange,
                     child: CircleAvatar(
-                      radius: 53,
-                      backgroundColor: Colors.orange,
-                      child: CircleAvatar(
-                        radius: 50.0,
-                        backgroundImage: Image.asset(
-                          "images/download.jpg",
-                          fit: BoxFit.cover,
-                        ).image,
-                        backgroundColor: Colors.transparent,
-                      ),
+                      radius: 50.0,
+                      backgroundImage: Image.asset(
+                        "images/download.jpg",
+                        fit: BoxFit.cover,
+                      ).image,
+                      backgroundColor: Colors.transparent,
                     ),
                   ),
-                  Column(
-                    spacing: 5,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Icon(Icons.person, size: 20, color: Colors.grey),
-                          Text(
-                            "Tushar Gupta",
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.developer_board_outlined,
-                            size: 20,
-                            color: Colors.grey,
-                          ),
-                          Text(
-                            "Flutter Developer",
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Icon(Icons.description, size: 20, color: Colors.grey),
-                          Text(
-                            "A young Hustler wants to learn Flutter",
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Container(
-                height: 2,
-                width: double.infinity,
-                color: Colors.orange,
-              ),
+                ),
+                Column(
+                  spacing: 5,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.person, size: 20, color: Colors.grey),
+                        Text(
+                          "Tushar Gupta",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.developer_board_outlined,
+                          size: 20,
+                          color: Colors.grey,
+                        ),
+                        Text(
+                          "Flutter Developer",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.description, size: 20, color: Colors.grey),
+                        Text(
+                          "A young Hustler wants to learn Flutter",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Container(height: 2, width: double.infinity, color: Colors.orange),
 
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
 
-                children: [
-                  Row(
-                    children: [
-                      Icon(Icons.mail, size: 20, color: Colors.grey),
-                      Text(
-                        "tushargupta@gmail.com",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.mail, size: 20, color: Colors.grey),
+                    Text(
+                      "tushargupta@gmail.com",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
 
-                  Row(
-                    children: [
-                      Icon(Icons.web, size: 20, color: Colors.grey),
-                      Text(
-                        "www.tushargupta.in",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
+                Row(
+                  children: [
+                    Icon(Icons.web, size: 20, color: Colors.grey),
+                    Text(
+                      "www.tushargupta.in",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
